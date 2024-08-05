d661c54aab26c36cbfcb0814eca6f342a98a2050
Stefanus Adri Irawan <148773999+stefanus-ai-tech@users.noreply.github.com>
2024-08-05 11:49:05 +0700

    Delete my-next-app directory

--- my-next-app/src/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// src/axiosInstance.ts
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'your-api-base-url',
-  // Add any other configurations you need
-});
-
-export default axiosInstance;
--- my-next-app/src/queryClient.ts
+++
@@ -1,6 +0,0 @@
-// src/queryClient.ts
-import {QueryClient} from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
-
-export default queryClient;
